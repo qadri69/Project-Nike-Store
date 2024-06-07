2023540363c1811df386f0337219dd2694925e87
ASUS <ASUS@DESKTOP-VOGCTEQ>
2024-06-07 16:17:59 +0800

    Add project files.

---
+++ Shop.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.Master" AutoEventWireup="true" CodeBehind="Shop.aspx.cs" Inherits="Project.Shop" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="Styles/StyleSheet1.css" rel="stylesheet" />
+</asp:Content>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="leftColumn">
+        <h3>Sales Item</h3>
+        <p>
+            Select Category:&nbsp;
+            <asp:DropDownList ID="ddlCategory" runat="server" DataSourceID="SqlDataSourceCategories" DataTextField="CatTitle" DataValueField="CatId" AutoPostBack="True"></asp:DropDownList>
+        </p>
+        <br />
+        <p>
+
+            <asp:GridView ID="GridViewProducts" runat="server" DataSourceID="SqlDataSourceProduct" AutoGenerateColumns="False" DataKeyNames="ProductID" OnSelectedIndexChanged="GridViewProducts_SelectedIndexChanged1">
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" />
+                    <asp:BoundField DataField="ProductID" HeaderText="ProductID" InsertVisible="False" ReadOnly="True" SortExpression="ProductID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Description" HeaderText="Description" SortExpression="Description" />
+                    <asp:BoundField DataField="Price" DataFormatString="{0:n2}" HeaderText="Price" SortExpression="Price" />
+                    <asp:ImageField DataImageUrlField="ImageUrl" DataImageUrlFormatString="Images/{0}" HeaderText="Images">
+                        <ControlStyle Width="70px" />
+                    </asp:ImageField>
+                </Columns>
+            </asp:GridView>
+
+        </p>
+        <p>
+            Products ID:
+            <asp:Label ID="lblProductsId" runat="server" ></asp:Label>&nbsp; |
+            Products Name:
+            <asp:Label ID="lblProductsName" runat="server" ></asp:Label>&nbsp; |
+            Products Price:
+            <asp:Label ID="lblProductsPrice" runat="server" ></asp:Label>&nbsp; |
+            Products Size:
+            <asp:DropDownList ID="ddlSize" runat="server">
+                <asp:ListItem Value="S">S</asp:ListItem>
+                <asp:ListItem Value="M">M</asp:ListItem>
+                <asp:ListItem Value="L">L</asp:ListItem>
+                <asp:ListItem Value="XL">XL</asp:ListItem>
+                
+            </asp:DropDownList>
+        </p>
+        <p>
+            Quantity:
+            <asp:TextBox ID="txtQuantity" runat="server" TextMode="Number" Width="50px">1</asp:TextBox>
+            <asp:Button ID="btnAddProduct" runat="server" Text="Button" OnClick="btnAddProduct_Click" />
+        </p>
+        <p>
+            <asp:Label ID="lblMessage1" runat="server" ></asp:Label>
+
+        </p>
+    </div>
+    <br />
+    <div class="rightColumn">
+        <h3>Sales Cart</h3>
+        <p>
+            Order ID:
+            <asp:Label ID="lblOrderId" runat="server" ></asp:Label>&nbsp; |
+            Date & Time:
+            <asp:Label ID="lblDateTime" runat="server" ></asp:Label>
+        </p>
+        <p>
+            <asp:GridView ID="GridViewCart" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSourceCart">
+                <Columns>
+                    <asp:BoundField DataField="ProductId" HeaderText="ProductId" SortExpression="ProductId" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Price" DataFormatString="{0:n2}" HeaderText="Price" SortExpression="Price" />
+                    <asp:BoundField DataField="Quantity" HeaderText="Quantity" SortExpression="Quantity" />
+                    <asp:BoundField DataField="Size" HeaderText="Size" SortExpression="Size" />
+                    <asp:BoundField DataField="Subtotal" DataFormatString="{0:n2}" HeaderText="Subtotal" ReadOnly="True" SortExpression="Subtotal" />
+                </Columns>
+            </asp:GridView>
+            Total amount:
+            <asp:Label ID="lblTotalAmountCart" runat="server" Text="RM0.00"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="btnConfirm" runat="server" Text="Confirm Order" OnClick="btnConfirm_Click" />&nbsp;
+            <asp:Button ID="btnCancel" runat="server" Text="Cancel Order" OnClick="btnCancel_Click" />&nbsp;
+            <asp:Button ID="btnNew" runat="server" Text="New Order" OnClick="btnNew_Click" />
+        </p>
+        <p>
+            <asp:Label ID="lblMessage2" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="lblTotalAmount" runat="server"></asp:Label><br />
+            <asp:Label ID="lblServiceTax" runat="server"></asp:Label><br />
+            <asp:Label ID="lblAmountAfterTax" runat="server"></asp:Label><br />
+            <asp:Label ID="lblRounding" runat="server"></asp:Label><br />
+            <asp:Label ID="lblAmountRounded" runat="server"></asp:Label><br />
+        </p>
+    </div>
+    <asp:SqlDataSource ID="SqlDataSourceCategories" runat="server" ConnectionString="<%$ ConnectionStrings:connNike %>" SelectCommand="SELECT * FROM [Categories] ORDER BY [CatTitle]"></asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSourceProduct" runat="server" ConnectionString="<%$ ConnectionStrings:connNike %>" SelectCommand="SELECT * FROM [Products] WHERE ([CatId] = @CatId)">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="ddlCategory" Name="CatId" PropertyName="SelectedValue" Type="Int32" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+    <asp:SqlDataSource ID="SqlDataSourceCart" runat="server" ConnectionString="<%$ ConnectionStrings:connNike %>" SelectCommand="spOrderGetItems" SelectCommandType="StoredProcedure">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="lblOrderId" Name="orderid" PropertyName="Text" Type="String" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+</asp:Content>
---
+++ Shop.aspx.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Project
+{
+    public partial class Shop : System.Web.UI.Page
+    {
+        static double totalAmount;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                GenerateSalesId();
+            }
+            
+        }
+
+        void GenerateSalesId()
+        {
+            string heckTicks = DateTime.Now.Ticks.ToString("X");
+            lblOrderId.Text = heckTicks.Substring(heckTicks.Length -15, 9);
+            lblDateTime.Text = DateTime.Now.ToString();
+            
+        }
+
+        protected void GridViewProducts_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+            lblProductsId.Text = GridViewProducts.SelectedRow.Cells[1].Text;
+            lblProductsName.Text = GridViewProducts.SelectedRow.Cells[2].Text;
+            lblProductsPrice.Text = GridViewProducts.SelectedRow.Cells[4].Text;
+        }
+
+        void OrderAddItem()
+        {
+            // Create connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["connNike"].ConnectionString);
+            // Create command object with Stored Procedure name
+            SqlCommand cmd = new SqlCommand("spOrderAddItem", conn);
+
+            // Set command object for stored procedure execution
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@orderid", lblOrderId.Text);
+            cmd.Parameters.AddWithValue("@productid", lblProductsId.Text);
+            cmd.Parameters.AddWithValue("@quantity", txtQuantity.Text);
+            cmd.Parameters.AddWithValue("@size", ddlSize.SelectedValue);
+            try
+            {
+                // Open connection
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                lblMessage1.Text = ex.Message;
+            }
+
+            finally
+            {
+                conn.Close();
+            }
+            txtQuantity.Text = "1";
+        }
+
+        void OrderGetTotalAmount()
+        {
+            // Create connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["connNike"].ConnectionString);
+
+            // Create command object with Stored Procedure name
+            SqlCommand cmd = new SqlCommand("spOrderGetTotalAmount", conn);
+
+            // Set command object for stored procedure execution
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@orderid", lblOrderId.Text);
+
+            try
+            {
+                // Open connection
+                conn.Open();
+                totalAmount = (double)cmd.ExecuteScalar();
+                lblTotalAmountCart.Text = totalAmount.ToString("RM 0.00");
+            }
+
+
+            catch (SqlException ex)
+            {
+                lblMessage2.Text = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+        protected void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            OrderAddItem();
+            GridViewCart.DataBind();
+            OrderGetTotalAmount();
+        }
+
+        void OrderConfirm()
+        {
+            // Create connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["connNike"].ConnectionString);
+
+            // Create command object with Stored Procedure name
+            SqlCommand cmd = new SqlCommand("spOrderConfirm", conn);
+            // Set command object for stored procedure execution
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@orderid", lblOrderId.Text);
+            try
+            {
+                // Open connection
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                lblMessage2.Text = "Order confirmed!";
+            }
+            catch (SqlException ex)
+            {
+                lblMessage2.Text = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            txtQuantity.Text = "1";
+        }
+
+        void DisplayInvoice()
+        {
+
+            double serviceTax, amountAfterTax, amountRounded, rounding;
+            serviceTax = totalAmount * 0.06;
+            amountAfterTax = totalAmount + serviceTax;
+            amountRounded = Math.Round((amountAfterTax) / 0.05) * 0.05;
+            rounding = amountRounded - amountAfterTax;
+
+            lblTotalAmount.Text = "Total amount: " + totalAmount.ToString("RM 0.00");
+            lblServiceTax.Text = "Service tax (6%): " + serviceTax.ToString("RM 0.00");
+            lblAmountAfterTax.Text = "Amount after tax: " + amountAfterTax.ToString("RM 0.00");
+            lblRounding.Text = "Rounding: " + rounding.ToString("RM 0.00");
+            lblAmountRounded.Text = "Amount to pay:" + amountRounded.ToString("RM 0.00");
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            OrderConfirm();
+            DisplayInvoice();
+        }
+
+        void ClearAll()
+        {
+            lblProductsId.Text = "";
+            lblProductsName.Text =
+            lblProductsPrice.Text = "";
+            lblTotalAmountCart.Text = "RM0.00";
+            lblTotalAmount.Text = "";
+            lblServiceTax.Text = "";
+            lblAmountAfterTax.Text =
+            lblRounding.Text = "";
+            lblAmountRounded.Text = "";
+            lblMessage1.Text = "";
+            lblMessage2.Text = "";
+        }
+
+        protected void btnNew_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+            GenerateSalesId();
+            ddlCategory.DataBind();
+            ddlSize.DataBind();
+            GridViewProducts.DataBind();
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            OrderRemoveNotConfirmed();
+            ClearAll();
+            GenerateSalesId();
+            ddlCategory.DataBind();
+            ddlSize.DataBind();
+            GridViewProducts.DataBind();
+        }
+
+        void OrderRemoveNotConfirmed()
+        {
+            // Create connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["connNike"].ConnectionString);
+            // Create command object with Stored Procedure name
+            SqlCommand cmd = new SqlCommand("spOrderRemoveNotConfirmed", conn);
+            // Set command object for stored procedure execution
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@orderid", lblOrderId.Text);
+            try
+            {
+                // Open connection
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+
+            catch (SqlException ex)
+            {
+                lblMessage2.Text = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
